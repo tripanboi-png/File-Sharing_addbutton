ba1fbdbcc11a6380407464ef5f1714e9df5da743
tripanboi-png <tripanboi@gmail.com>
2026-05-31 00:28:51 +0700

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -45,25 +45,47 @@ async def subsgroup(filter, client, update):
 
 
 async def is_subscribed(filter, client, update):
-    if not FORCE_SUB_CHANNEL:
-        return True
-    if not FORCE_SUB_GROUP:
-        return True
+
     user_id = update.from_user.id
+
     if user_id in ADMINS:
         return True
-    try:
-        member = await client.get_chat_member(chat_id=FORCE_SUB_GROUP, user_id=user_id)
-    except UserNotParticipant:
-        return False
-    try:
-        member = await client.get_chat_member(
-            chat_id=FORCE_SUB_CHANNEL, user_id=user_id
-        )
-    except UserNotParticipant:
-        return False
 
-    return member.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER]
+    if FORCE_SUB_CHANNEL:
+        try:
+            member = await client.get_chat_member(
+                FORCE_SUB_CHANNEL,
+                user_id
+            )
+
+            if member.status not in [
+                ChatMemberStatus.OWNER,
+                ChatMemberStatus.ADMINISTRATOR,
+                ChatMemberStatus.MEMBER
+            ]:
+                return False
+
+        except UserNotParticipant:
+            return False
+
+    if FORCE_SUB_GROUP:
+        try:
+            member = await client.get_chat_member(
+                FORCE_SUB_GROUP,
+                user_id
+            )
+
+            if member.status not in [
+                ChatMemberStatus.OWNER,
+                ChatMemberStatus.ADMINISTRATOR,
+                ChatMemberStatus.MEMBER
+            ]:
+                return False
+
+        except UserNotParticipant:
+            return False
+
+    return True
 
 
 async def encode(string):
