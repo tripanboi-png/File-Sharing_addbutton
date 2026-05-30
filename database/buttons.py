973026bc16ea5b6260604a4b2f3b299284ef5b3a
tripanboi-png <tripanboi@gmail.com>
2026-05-31 00:51:55 +0700

    Update buttons.py

--- database/buttons.py
+++ database/buttons.py
@@ -7,19 +7,36 @@ db = client["FileSharingBot"]
 buttons = db["buttons"]
 
 
-async def add_button(name, url, chat_id):
-    buttons.insert_one({
-        "name": name,
-        "url": url,
-        "chat_id": chat_id
-    })
+async def add_button(name, url, chat_id=None):
+    buttons.insert_one(
+        {
+            "name": name,
+            "url": url,
+            "chat_id": chat_id
+        }
+    )
 
 
 async def delete_button(name):
-    buttons.delete_one({
-        "name": name
-    })
+    buttons.delete_one(
+        {
+            "name": name
+        }
+    )
 
 
 async def get_buttons():
     return list(buttons.find())
+
+
+async def get_join_buttons():
+    return list(
+        buttons.find(
+            {
+                "chat_id": {
+                    "$exists": True,
+                    "$ne": None
+                }
+            }
+        )
+    )
