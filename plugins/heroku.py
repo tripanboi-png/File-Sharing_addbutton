dcab7bbd2885f9e8ad24ea4ae44e084092eccaf0
tripanboi-png <tripanboi@gmail.com>
2026-05-30 20:29:48 +0700

    Update heroku.py

--- plugins/heroku.py
+++ plugins/heroku.py
@@ -19,9 +19,10 @@ import dotenv
 import heroku3
 import urllib3
 from bot import Bot
-from config import ADMINS, HEROKU_API_KEY, HEROKU_APP_NAME
+from config import HEROKU_API_KEY, HEROKU_APP_NAME
 from pyrogram import filters
 from pyrogram.types import Message
+from database.admins import is_admin
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 if HEROKU_APP_NAME is not None and HEROKU_API_KEY is not None:
@@ -52,10 +53,11 @@ async def is_heroku():
     return "heroku" in socket.getfqdn()
 
 
-@Bot.on_message(filters.command("getvar") & filters.user(ADMINS))
-async def varget_(client: Bot, message: Message):
-    if len(message.command) != 2:
-        return await message.reply_text("<b>Usage:</b>\n/getvar [Var Name]")
+@Bot.on_message(filters.command("getvar"))
+async def varget_(client, message):
+
+    if not await is_admin(message.from_user.id):
+        return message.reply_text("<b>Usage:</b>\n/getvar [Var Name]")
     check_var = message.text.split(None, 2)[1]
     if await is_heroku():
         if HAPP is None:
@@ -110,10 +112,11 @@ async def vardel_(client: Bot, message: Message):
             os.system(f"kill -9 {os.getpid()} && bash start")
 
 
-@Bot.on_message(filters.command("setvar") & filters.user(ADMINS))
-async def set_var(client: Bot, message: Message):
-    if len(message.command) < 3:
-        return await message.reply_text("<b>Usage:</b>\n/setvar [Var Name] [Var Value]")
+@Bot.on_message(filters.command("getvar"))
+async def varget_(client: Bot, message: Message):
+
+    if not await is_admin(message.from_user.id):
+        return message.reply_text("<b>Usage:</b>\n/setvar [Var Name] [Var Value]")
     to_set = message.text.split(None, 2)[1].strip()
     value = message.text.split(None, 2)[2].strip()
     if await is_heroku():
