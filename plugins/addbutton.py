ea2b4757dc8a5851df57349feab37ecfa367219d
tripanboi-png <tripanboi@gmail.com>
2026-05-30 23:43:53 +0700

    Update addbutton.py

--- plugins/addbutton.py
+++ plugins/addbutton.py
@@ -1,3 +1,9 @@
-/addbutton
-/delbutton
-/buttons
+from pyrogram import filters
+from pyrogram.types import Message
+
+from bot import Bot
+
+
+@Bot.on_message(filters.command("buttons"))
+async def buttons_cmd(client, message: Message):
+    await message.reply_text("Fitur sedang dibuat")
